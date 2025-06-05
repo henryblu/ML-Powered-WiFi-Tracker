d36bba8db40413ca5b9b5f9624ecbe10425c25f5
Henry Blue <95654836+henryblu@users.noreply.github.com>
2025-06-05 16:43:26 +0200

    Fix unit test build

--- _components/time_component.h
+++ _components/time_component.h
@@ -1,21 +1,22 @@
 #ifndef ESP32_CSI_TIME_COMPONENT_H
 #define ESP32_CSI_TIME_COMPONENT_H
 
-#include <chrono>
 #include <sys/time.h>
+#include <time.h>
+#include <stdbool.h>
 
 static char *SET_TIMESTAMP_SIMPLE_TEMPLATE = (char *) "%li.%li";
 static char *SET_TIMESTAMP_TEMPLATE = (char *) "SETTIME: %li.%li";
 
 bool real_time_set = false;
 
-bool match_set_timestamp_template(char *candidate_string) {
+static inline bool match_set_timestamp_template(char *candidate_string) {
     long int tv_sec;
     long int tv_usec;
     return sscanf(candidate_string, SET_TIMESTAMP_TEMPLATE, &tv_sec, &tv_usec) > 0;
 }
 
-void time_set(char *timestamp_string) {
+static inline void time_set(char *timestamp_string) {
     long int tv_sec;
     long int tv_usec;
 
@@ -31,14 +32,16 @@ void time_set(char *timestamp_string) {
     }
 }
 
-double get_system_clock_timestamp() {
-    // returns timestamp in seconds (with decimal places)
-    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count() / 1000000.0;
+static inline double get_system_clock_timestamp() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
 
-double get_steady_clock_timestamp() {
-    // returns timestamp in seconds (with decimal places)
-    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() / 1000000.0;
+static inline double get_steady_clock_timestamp() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec / 1000000000.0;
 }
 
 #endif //ESP32_CSI_TIME_COMPONENT_H
