06a25480e853bdd7d4f22b3a7a269f106a363a9c
henryblu <henry.s.blue@gmail.com>
2025-06-18 13:43:33 +0300

    linting

--- main/main.cc
+++ main/main.cc
@@ -75,7 +75,7 @@ void passive_init() {
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    // Capture CSI from all WiFi frame types to maximize sample rate
+    // Capture CSI from all WiFi frame types to get only data frames
     const wifi_promiscuous_filter_t filt = {
             .filter_mask = WIFI_PROMIS_FILTER_MASK_DATA
     };
